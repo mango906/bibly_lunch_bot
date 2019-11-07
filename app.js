569c20ba045ba5b3e9b6b8d64491612547cfbe90
mango906 <minkb01@gmail.com>
2019-11-07 12:09:10 +0900

    [ADD] 점심 삭제 기능 추가

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const { RTMClient } = require("@slack/client");
 
-const botAPIToken = "xoxb-7569711284-823584234279-F9jHYl8EOJ1USMeDUsB38AmD";
+const botAPIToken = "xoxb-7569711284-823584234279-xPoQEkUaG2Be4q8EldGcXZI2";
 
 const token = process.env.SLACK_TOKEN || botAPIToken;
 
+const rtm = new RTMClient(token);
 
 let data = [
   {
+    id: 1,
     name: "던킨도너츠",
     price: "1500"
+  },
+  {
+    id: 2,
+    name: "신메뉴",
+    price: "99999"
   }
 ];
 
@@ -19,14 +26,11 @@ rtm.on("message", message => {
 
   if (text === "!점심 목록") {
     rtm.sendMessage(dataToText(), message.channel);
-  } else if (text === "!점심 추가") {
-    const data = [];
+  } else if (text.includes("!점심 추가")) {
   } else if (text.includes("!점심 삭제")) {
-    const idx = text.replace("!점심 삭제 ", "");
-    console.log(idx);
-    console.log(data.length);
-    if (idx <= data.length) {
-      data = data.filter((el, i) => i !== idx);
+    const id = parseInt(text.replace("!점심 삭제 ", ""));
+    if (id <= data.length && id > 0) {
+      data = data.filter(el => el.id !== id);
       rtm.sendMessage("메뉴를 삭제했어요. 삐빅", message.channel);
     } else {
       rtm.sendMessage("그런건 없는데요...?", message.channel);
@@ -36,8 +40,10 @@ rtm.on("message", message => {
 
 function dataToText() {
   let msg = "";
-  data.forEach((el, i) => {
-    const text = `${i + 1}. 이름 : ${el.name}, 가격 : ${el.price} \n`;
+  if (!data.length) return "점심이 없어요.";
+
+  data.forEach(el => {
+    const text = `${el.id}. 이름 : ${el.name}, 가격 : ${el.price} \n`;
     msg += text;
   });
 
