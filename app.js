a9a06d23f215346779297e5c008c53617ed6e817
mango906 <minkb01@gmail.com>
2019-11-07 14:53:59 +0900

    [FIX] 슬랙에서 더 보기 쉽게 수정

--- app.js
+++ app.js
@@ -6,14 +6,8 @@ const rtm = new RTMClient(token);
 
 let data = [
   {
-    id: 1,
-    name: "던킨도너츠",
-    price: "1500"
-  },
-  {
-    id: 2,
-    name: "신메뉴",
-    price: "99999"
+    name: "왕장",
+    price: 5000
   }
 ];
 
@@ -22,18 +16,35 @@ rtm.start();
 rtm.on("message", message => {
   var text = message.text;
 
-  if (text === "!점심 목록") {
+  if (text === "!점심목록") {
     rtm.sendMessage(dataToText(), message.channel);
-  } else if (text.includes("!점심 추가")) {
-  } else if (text.includes("!점심 삭제")) {
-    const id = parseInt(text.replace("!점심 삭제 ", ""));
+  } else if (text.includes("!점심추가")) {
+    const value = text.replace("!점심추가 ", "");
+    const item = value.split(", ");
+    if (item.length !== 2) {
+      rtm.sendMessage("형식을 지켜주세요.", message.channel);
+      return;
+    }
+    try {
+      const newItem = {
+        name: item[0],
+        price: item[1]
+      };
+      data = [...data, newItem];
+    } catch (err) {
+      rtm.sendMessage("알수 없는 에러 발생 삐빠뿌", message.channel);
+    }
+  } else if (text.includes("!점심삭제")) {
+    const id = parseInt(text.replace("!점심삭제 ", ""));
     if (id <= data.length && id > 0) {
-      data = data.filter(el => el.id !== id);
+      data = data.filter(el => data.indexOf(el) + 1 !== id);
+      // data = data.filter(el => el.id !== id);
       rtm.sendMessage("메뉴를 삭제했어요. 삐빅", message.channel);
     } else {
       rtm.sendMessage("그런건 없는데요...?", message.channel);
     }
   } else if (text === "!점심") {
+    if (!data.length) rtm.sendMessage("점심이 없어요", message.channel);
     rtm.sendMessage(randomItem(), message.channel);
   }
 });
@@ -42,8 +53,8 @@ function dataToText() {
   let msg = "";
   if (!data.length) return "점심이 없어요.";
 
-  data.forEach(el => {
-    msg += ObjectToText(el);
+  data.forEach((el, i) => {
+    msg += `\`${i + 1}. ${ObjectToText(el)} \` \n`;
   });
 
   return msg;
@@ -55,5 +66,5 @@ function randomItem() {
 }
 
 function ObjectToText(obj) {
-  return `${obj.id}. 이름 : ${obj.name}, 가격 : ${obj.price} \n`;
+  return `이름 : ${obj.name}, 가격 : ${obj.price}`;
 }
